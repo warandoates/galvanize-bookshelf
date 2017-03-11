1bb3f611cbd5a9c5d447d554c35539ee248be9a0
Michael Martinez <martinez1212@gmail.com>
2017-03-11 10:43:27 -0800

    successfully created favorites migration table; passing migrations test

---
+++ migrations/20170311101023_favorites.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments('id').primary();
+    table.integer('book_id').references('id').inTable('books')
+    .notNullable().onDelete('cascade');
+    table.integer('user_id').references('id').inTable('users')
+    .notNullable().onDelete('cascade');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('favorites');
+};
